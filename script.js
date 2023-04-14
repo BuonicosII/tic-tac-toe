c7ec6aeb8b6e7d6b615333a2593bf79ab1cc71d9
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-04-14 19:02:11 +0200

    Add function to add marker to board and array
    Add function to place player marker on the board as textcontent and
    change the corresponding value in the gameboard array

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 //module which initializes the gameboard
 const Gameboard = (() => {
-    const gameboard = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
+    const gameboard = ['', '', '', '', '', '', '', '', ''];
     const table = document.querySelector('#table');
+
+    function choiceOnBoard (event) {
+        event.target.textContent = `${player.marker}`;
+        gameboard.splice(event.target.getAttribute("data-index"), 1, `${player.marker}`);
+    }
 
     function initializeBoard () {
         for (const [index, value] of gameboard.entries()) {
             const oneCell = document.createElement('div');
             oneCell.setAttribute('class', 'cell');
-            oneCell.setAttribute('data-index', `${index + 1}`);
+            oneCell.setAttribute('data-index', `${index}`);
             oneCell.textContent = value;
+            oneCell.addEventListener('click', choiceOnBoard);
             table.appendChild(oneCell);
         }
     }
 
     initializeBoard();
+
+    return gameboard;
 })();
 
 //factory function to create players
